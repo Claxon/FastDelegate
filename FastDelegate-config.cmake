c84a1a9849c1aec589cc9e3f4834f754dcbba8a5
Christopher Beckford <christopher.beckford@foundry.com>
2019-06-27 00:25:31 +0100

    Ading CMakeLists.txt

--- FastDelegate-config.cmake
+++ FastDelegate-config.cmake
@@ -1,4 +1,4 @@
 add_library(FastDelegate INTERFACE IMPORTED GLOBAL)
-set_target_properties(RapidJSON::RapidJSON PROPERTIES
+set_target_properties(FastDelegate::FastDelegate PROPERTIES
     INTERFACE_INCLUDE_DIRECTORIES "${CMAKE_CURRENT_SOURCE_DIR}/include"
 )
