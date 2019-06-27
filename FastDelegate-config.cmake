559e114bb28dad3dd5ae70270753625579218ede
Christopher Beckford <christopher.beckford@foundry.com>
2019-06-27 22:06:22 +0100

    Updating cmake config

--- FastDelegate-config.cmake
+++ FastDelegate-config.cmake
@@ -1,4 +1,4 @@
 add_library(FastDelegate INTERFACE IMPORTED GLOBAL)
 set_target_properties(FastDelegate::FastDelegate PROPERTIES
-    INTERFACE_INCLUDE_DIRECTORIES "${CMAKE_CURRENT_SOURCE_DIR}/include"
+    INTERFACE_INCLUDE_DIRECTORIES "${CMAKE_CURRENT_SOURCE_DIR}/include/FastDelegate"
 )
